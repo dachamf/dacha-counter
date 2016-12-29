6e238d93ed265b1f2ccd31eee337603aaedbe8fc
dalibor <dalibor.djordjevic@stanga.net>
2016-12-29 19:54:23 +0100

    Update styles

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -105,22 +105,6 @@
     </div>
 </footer>
 
-
-<!--<footer class="footer">-->
-<!---->
-<!--    <div class="row full-width">-->
-<!---->
-<!--        <div class="small-12 medium-12 large-12 columns">-->
-<!---->
-<!--            -->
-<!---->
-<!--        </div>-->
-<!---->
-<!--    </div>-->
-<!---->
-<!--</footer>-->
-
-
 <!--Include js files-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="../assets/js/materialize.min.js"></script>
--- app/views/home/report.php
+++ app/views/home/report.php
@@ -3,41 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <title>Dachas save the sel</title>
-    <link rel="stylesheet" href="../assets/css/foundation.css">
-    <link rel="stylesheet" href="../assets/css/foundation-datepicker.css">
-    <link rel="stylesheet" href="../assets/css/app.css">
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <!-- For Foundation Icons -->
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+
+    <!-- Google fonts -->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--  Material css  -->
+    <link rel="stylesheet" href="../assets/css/materialize.min.css">
+    <!--  Custom CSS  -->
+    <link rel="stylesheet" href="../assets/app.css">
 </head>
 <body>
-<!--Headder-->
-<header class="header">
-    <h1 class="headline">Dachas Save the Sale app <br>
-        <small>by Dacha2</small>
-    </h1>
-    <ul class="header-subnav">
-        <li><a href="/home/index"><i class="fi-home"></i> Home</a></li>
-        <li><a href="/home/report" class="is-active"> Reports</a></li>
-    </ul>
-
-</header>
+<nav>
+    <div class="nav-wrapper">
+        <a href="/" class="brand-logo">Dachas Save the Sale app <br>
+            <small>by Dacha2</small>
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="/home/index" class="is-active"><i class="material-icons">home</i></a></li>
+            <li><a href="/home/report"><i class="material-icons">format_list_numbered</i></a></li>
+        </ul>
+    </div>
+</nav>
 <!-- End Header -->
 <!--Main Content-->
-<div class="main-content">
-
+<div class="container content">
+    <!--            <div class="col s6">-->
+    <!--                <input type="date" class="datepicker">-->
+    <!--            </div>-->
     <form action="report">
         <div class="row">
-            <div class="large-6 columns">
+            <div class="col s6">
                 <label for="from">FROM</label>
-                <input type="date" name="from" id="from" placeholder="Choose 'FROM' date">
+                <input type="date" class="datepicker" id="from">
             </div>
-            <div class="large-6 columns">
+            <div class="col s6">
                 <label for="to">TO</label>
-                <input type="date" name="to" id="to" placeholder="Choose 'TO' date">
+                <input type="date" class="datepicker" id="to">
             </div>
-            <div class="large6 columns">
-                <input type="submit" class="button" value="Get Report">
+            <div class="col s6">
+                <input type="submit" class="waves-effect waves-light btn z-depth-5" value="Get Report">
             </div>
         </div>
     </form>
@@ -46,7 +49,7 @@
     <div class="row">
         <div class="large12 columns">
             <?php if (isset($data['sales'])): ?>
-                <table>
+                <table class="striped z-depth-1">
                     <thead>
                     <tr>
                         <th>Name</th>
@@ -73,34 +76,45 @@
                 </table>
             <?php endif; ?>
         </div>
+        <div class="col s6 padding">
+            <input type="submit" class="waves-effect waves-light btn z-depth-5" value="Get excel file">
+        </div>
     </div>
 </div>
 
 
 <!-- Footer -->
 
-<footer class="footer">
-
-    <div class="row full-width">
-
-        <div class="small-12 medium-12 large-12 columns">
-
-            Code and Design by: <a href="mailto:dachamf@gmail.com"> dacha</a>
-
+<footer class="page-footer z-depth-5">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">Coded and designed by: <a href="mailto:dachamf@gmail.com">dacha</a>
+                </p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="/home/index">Home</a></li>
+                    <li><a class="grey-text text-lighten-3" href="/home/report">Reports</a></li>
+                </ul>
+            </div>
         </div>
-
     </div>
-
+    <div class="footer-copyright">
+        <div class="container">
+            © 2016 All rights reserved
+            <a class="grey-text text-lighten-4 right" href="https://www.facebook.com/dalibor.djordjevic">Contact me <i
+                        class="material-icons">tag_faces</i></a>
+        </div>
+    </div>
 </footer>
 
 <!--Include js files-->
-<script type="application/javascript" src="../assets/js/vendor/jquery.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script type="application/javascript" src="../assets/js/vendor/what-input.js"></script>
-<script type="application/javascript" src="../assets/js/vendor/foundation.min.js"></script>
-<script type="application/javascript" src="../assets/js/vendor/foundation-datepicker.min.js"></script>
-<script type="application/javascript" src="../assets/js/app.js"></script>
-
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="../assets/js/materialize.min.js"></script>
+<script type="text/javascript" src="../assets/app.js"></script>
 </body>
 </html>
 
